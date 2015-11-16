6f73a6c52f0ca7a8872701c4aba96f421dc2f459
Matej <mat.per.vt@gmail.com>
2015-11-16 22:45:05 +0100

    Deleted unused LM74TemperatureSensor constructor with single SpiConnection atribute

--- bulldog-devices/src/main/java/io/silverspoon/bulldog/devices/sensors/LM74TemperatureSensor.java
+++ bulldog-devices/src/main/java/io/silverspoon/bulldog/devices/sensors/LM74TemperatureSensor.java
@@ -17,10 +17,11 @@ import io.silverspoon.bulldog.core.util.BulldogUtil;
 public class LM74TemperatureSensor extends SpiDevice {
    DigitalOutput digitalOutput = null;
 
-   public LM74TemperatureSensor(SpiConnection connection) {
-      super(connection);
-   }
-
+   /**
+    * Constructor
+    * @param connection
+    * @param digitalOutput
+    */
    public LM74TemperatureSensor(SpiConnection connection, DigitalOutput digitalOutput) {
       super(connection);
       this.digitalOutput = digitalOutput;
